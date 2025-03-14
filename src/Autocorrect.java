37eca33bc835e36445c649599bff8233b8c938d3
BeckettPorter <porterbeckett@gmail.com>
2025-03-14 13:24:28 -0700

    fixed edit distance and started on hashing

--- src/Autocorrect.java
+++ src/Autocorrect.java
@@ -16,6 +16,7 @@ public class Autocorrect {
     private final int threshold;
     private final String[] dictionary;
     public static final int RADIX = 16;
+    public static String targetWord;
 
     /**
      * Constucts an instance of the Autocorrect class.
@@ -27,8 +28,98 @@ public class Autocorrect {
         this.threshold = threshold;
         this.dictionary = words;
 
-        runTest("");
+        this.targetWord = "Algorithmm";
 
+//        runTest("");
+    }
+
+    public static void main(String[] args)
+    {
+
+    }
+
+    private String[] getPossibleCandidates(int numCandidatesToGet)
+    {
+
+        ArrayList<Long>[] array = new ArrayList[dictionary.length];
+        int nGramSize = 2;
+
+        // Arraylist (dictionary size) of Arraylists (n -gram)
+
+        for (int i = 0; i < dictionary.length; i++)
+        {
+            String currentWord = dictionary[i];
+
+            array[i] = new ArrayList<>();
+
+            for (int j = 0; j < currentWord.length(); j += nGramSize)
+            {
+                String currentSubstring = currentWord.substring(j, j + nGramSize);
+
+                array[i].add(hashSingleString(currentSubstring));
+            }
+
+        }
+
+        for (int i = 0; i < dictionary.length; i++)
+        {
+
+            if ()
+            array[i]
+        }
+
+        return
+    }
+
+// Helper method that hashes a single string using the Rabin-Karp algorithm.
+    private long hashSingleString(String str)
+    {
+        int hash = 0;
+
+        for (int i = 0; i < str.length(); i++)
+        {
+            hash = (hash * RADIX + str.charAt(i)) % 1610612741;
+        }
+        return hash;
+    }
+
+    // We are trying to turn word 1 into word 2
+    private int findEditDistance(String word1, String word2, int index1, int index2)
+    {
+        // Base cases that check if we have reached the end of either word. If we have, then we add
+        // the difference between the other word's length and what index it is on to the returned edit distance.
+        if (index1 == word1.length())
+        {
+            return word2.length() - index2;
+        }
+        if (index2 == word2.length())
+        {
+            return word1.length() - index1;
+        }
+
+        char char1 = word1.charAt(index1);
+        char char2 = word2.charAt(index2);
+
+        // We have three possible choices, we can add a letter, remove a letter, or replace a letter.
+        if (char1 != char2)
+        {
+            // replace
+            int replaceCount = 1 + findEditDistance(word1, word2, index1 + 1, index2 + 1);
+
+            // add
+            int addCount = 1 + findEditDistance(word1, word2, index1, index2 + 1);
+
+            // remove
+            int removeCount = 1 + findEditDistance(word1, word2, index1 + 1, index2);
+
+            // Return the lowest we've found.
+            return Math.min(Math.min(replaceCount, addCount), removeCount);
+        }
+        else
+        {
+            // Otherwise if the two chars are equal, just increment both indexes.
+            return findEditDistance(word1, word2, index1 + 1, index2 + 1);
+        }
     }
 
     /**
@@ -37,105 +128,34 @@ public class Autocorrect {
      * @return An array of all dictionary words with an edit distance less than or equal
      * to threshold, sorted by edit distance, then sorted alphabetically.
      */
-    public String[] runTest(String typed)
-    {
-        ArrayList<String> ar = new ArrayList<>();
+    public String[] runTest(String typed) {
+        ArrayList<String> potentialWordsAr = new ArrayList<>();
 
+        // Go through each word in the dictionary and add it to the arrayList if it is within
+        // the threshold for edit distance.
         for (String s : dictionary)
         {
-            if (findEditDistance(s, typed, 0) <= threshold)
+            if (findEditDistance(s, typed, 0, 0) <= threshold)
             {
-                ar.add(s);
+                potentialWordsAr.add(s);
             }
         }
 
-        String[] potentialWords = new String[ar.size()];
+        // Code I got online that sorts my arraylist by edit distance and alphabetically.
+        potentialWordsAr.sort((s1, s2) -> {
+            int distance1 = findEditDistance(s1, typed, 0, 0);
+            int distance2 = findEditDistance(s2, typed, 0, 0);
+            return (distance1 != distance2) ? Integer.compare(distance1, distance2) : s1.compareTo(s2);
+        });
 
-        ar.toArray(potentialWords);
+
+        // Convert the arrayList to a normal string array and return it.
+        String[] potentialWords = new String[potentialWordsAr.size()];
+
+        potentialWordsAr.toArray(potentialWords);
 
         return potentialWords;
     }
-
-//    private String[] getPossibleCandidates(int numCandidatesToGet)
-//    {
-//
-//        ArrayList<Long> ar = new ArrayList<>();
-//
-//        // Arraylist (dictionary size) of Arraylists (n -gram)
-//
-//
-//        return
-//    }
-//
-// Helper method that hashes a single string using the Rabin-Karp algorithm.
-//    private long hashSingleString(String str)
-//    {
-//        int hash = 0;
-//
-//        for (int i = 0; i < str.length(); i++)
-//        {
-//            hash = (hash * RADIX + str.charAt(i)) % 1610612741;
-//        }
-//        return hash;
-//    }
-
-    // We are trying to turn word 1 into word 2
-    private int findEditDistance(String word1, String word2, int index)
-    {
-        if (index >= word1.length() || index >= word2.length())
-        {
-            return Math.abs(word1.length() - word2.length());
-        }
-
-        char char1 = word1.charAt(index);
-        char char2 = word2.charAt(index);
-
-        // We have three possible choices, we can add a letter, remove a letter, or replace a letter.
-
-        // First, given a char, we have to see which options we can do.
-
-        if (char1 != char2)
-        {
-            // REPLACE
-            String replaceString = word1.substring(0, index) + char2 + word1.substring(index + 1);
-
-            int replaceCount = findEditDistance(replaceString, word2, index + 1);
-
-            // ADD
-            String addString = word1.substring(0, index) + char2 + word1.substring(index);
-
-            int addCount = findEditDistance(addString, word2, index + 1);
-
-            // REMOVE
-            String removeString = word1.substring(0, index) + word1.substring(index + 1);
-
-            int removeCount = findEditDistance(removeString, word2, index + 1);
-
-
-            // Return the lowest we've found.
-            return 1 + Math.min(Math.min(replaceCount, addCount), removeCount);
-        }
-        else
-        {
-            return findEditDistance(word1, word2, index + 1);
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     /**
      * Loads a dictionary of words from the provided textfiles in the dictionaries' directory.
